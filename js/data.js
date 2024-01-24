a908d4dcd68aa0cb52556a88a3bbcfa57a5b259a
Hyojin Seo <hyojinseo0903@gmail.com>
2024-01-24 15:59:53 -0800

    example images and questions

---
+++ js/data.js
@@ -0,0 +1,99 @@
+export const questions = [
+  {
+    number: '01',
+    question: 'Example question 1?',
+    choices: [
+      { text: 'Choice 1A', value: 'a' },
+      { text: 'Choice 1B', value: 'b' }
+    ]
+  },
+  {
+    number: '02',
+    question: 'Example question 2?',
+    choices: [
+      { text: 'Choice 2A', value: 'a' },
+      { text: 'Choice 2B', value: 'b' }
+    ]
+  },
+  {
+    number: '03',
+    question: 'Example question 3?',
+    choices: [
+      { text: 'Choice 3A', value: 'a' },
+      { text: 'Choice 3B', value: 'b' }
+    ]
+  },
+  {
+    number: '04',
+    question: 'Example question 4?',
+    choices: [
+      { text: 'Choice 4A', value: 'a' },
+      { text: 'Choice 4B', value: 'b' }
+    ]
+  }
+]
+
+export const results = [
+  {
+    title: '1',
+    character: '/Images/result1.jpeg',
+    results: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    jobs: ['', ''],
+    lectureImg: '/Images/result1.jpeg',
+    lectureUrl: 'https://hyojinseo.github.io/'
+  },
+  {
+    title: '2',
+    character: '/Images/result2.jpeg',
+    results: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    jobs: ['', ''],
+    lectureImg: '/Images/result2.jpeg',
+    lectureUrl: 'https://hyojinseo.github.io/'
+  },
+  {
+    title: '3',
+    character: '/Images/result3.jpeg',
+    results: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    jobs: ['', ''],
+    lectureImg: '/Images/result3.jpeg',
+    lectureUrl: 'https://hyojinseo.github.io/'
+  },
+  {
+    title: '4',
+    character: '/Images/result4.jpeg',
+    results: [
+      '',
+      '',
+      '',
+      ''
+    ],
+    jobs: ['', ''],
+    lectureImg: '/Images/result4.jpeg',
+    lectureUrl: 'https://hyojinseo.github.io/'
+  }
+]
+export const mbtis = {
+  aaa: 0,
+  aaab: 1,
+  aaba: 2,
+  aabb: 3,
+  baaa: 0,
+  baab: 1,
+  baba: 2,
+  babb: 3
+};
